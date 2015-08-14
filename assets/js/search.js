11a778d91c79657f92eb9dbc8e7973249950b187
hanzel.jesheen <hanzelj@gmail.com>
2015-08-14 23:10:24 +0530

    search from api

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,17 +1,40 @@
 var form = document.getElementById('searchform'),
 	box = document.getElementById('searchbox'),
 	span = document.getElementById('query'),
-	query = location.hash.slice(1);
+	list = document.getElementById('search-list'),
+	query = location.hash.slice(1),
+	xhr = new XMLHttpRequest();
 
-function searchFrm(e) {
+function searchFrm (e) {
 	e.preventDefault();
 	if (box.value.trim() !== '') {
 		search(box.value.trim().split(' ').join('+'));
 	}
 }
 
+function render (results) {
+	if (!results.length) {
+		list.innerHTML = '<span class="text">Sorry, No results.</span>';
+		return;
+	}
+	list.innerHTML = "";
+	for (var i=0; i<results.length; i++) {
+		list.innerHTML += '<li><a class="post-link" href="/stories/'+results[i].id+'">'+results[i].title+'</a><p>'+results[i].summary+'</p></li>';
+	}
+}
+
 function search (query) {
 	span.innerHTML = query.split('+').join(' ');
+	location.hash = query;
+	list.innerHTML = '<span class="text">Loading results...</span>';
+
+	xhr.open('GET', encodeURI('http://api.botleg.com/search/'+query));
+	xhr.onload = function() {
+		if (xhr.status === 200) {
+			render(JSON.parse(xhr.responseText));
+		}
+	};
+	xhr.send();
 }
 
 box.value = span.innerHTML = query.split('+').join(' ');
@@ -22,12 +45,3 @@ if(form.addEventListener){
 } else if(form.attachEvent){
 	form.attachEvent('onsubmit', searchFrm);
 }
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', encodeURI('http://api.botleg.com/search/'+query));
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        console.log(JSON.parse(xhr.responseText));
-    }
-};
-xhr.send();
