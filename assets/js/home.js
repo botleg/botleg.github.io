5d1d7d6d824ed95f44386ade09a126d29029e2ec
hanzel.jesheen <hanzelj@gmail.com>
2015-08-31 19:37:48 +0530

    change in js file root folder

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,14 +1,1 @@
-var ele = document.getElementById('searchform');
-
-function search(e) {
-	e.preventDefault();
-	if (document.getElementById('searchbox').value.trim() !== '') {
-		window.location.href = '/search/#' + document.getElementById('searchbox').value.trim().split(' ').join('+');
-	}
-}
-
-if(ele.addEventListener){
-	ele.addEventListener("submit", search, false);
-} else if(ele.attachEvent){
-	ele.attachEvent('onsubmit', search);
-}
+function search(e){e.preventDefault(),""!==document.getElementById("searchbox").value.trim()&&(window.location.href="/search/#"+document.getElementById("searchbox").value.trim().split(" ").join("+"))}var ele=document.getElementById("searchform");ele.addEventListener?ele.addEventListener("submit",search,!1):ele.attachEvent&&ele.attachEvent("onsubmit",search);
